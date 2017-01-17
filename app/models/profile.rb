798e4adc8514ef683400e67961d5d625dd6baf57
Pablo M. Guevara <pablomguevara@gmail.com>
2017-01-17 12:58:21 -0300

    before submiting all test pased

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,35 @@
+
+
 class Profile < ActiveRecord::Base
-  belongs_to :user
+    belongs_to :user
+
+    validates :gender, inclusion: ["male", "female"]
+
+    validate :profile_name_lastname_validation, :male_not_sue
+
+    def profile_name_lastname_validation
+        if first_name.nil? and last_name.nil?
+            errors.add(:first_name, "Name and Lastname cannot both be nil")
+            errors.add(:last_name, "Name and Lastname cannot both be nil")
+        end
+    end
+
+    def male_not_sue
+        if gender == "male"
+            if first_name == "Sue"
+                errors.add(:first_name, "Male can not be named Sue")
+            end
+        end
+    end
+
+    # Add a **class** method to the `Profile` class, called `get_all_profiles`, which:
+    # * accepts a min and max for the `birth year`
+    # * issues a `BETWEEN` SQL clause in a `where` clause to locate `Profiles` 
+    # with birth years that are between min year and max year 
+    # * defends itself against SQL injection when applying the parameters to the SQL clauses
+    # * returns a collection of Profiles in ASC birth year order
+    scope :get_all_profiles, -> (min, max){
+    	where("birth_year BETWEEN :min AND :max", :min => min, :max => max).order(birth_year: :asc)
+    }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :todo_items
   resources :profiles
   resources :todo_lists
   resources :users
